ac498ac0a6096efd9a0de079a6f44de5bad3e630
Eugene Chong <eugenechong.chills@gmail.com>
2013-04-02 02:26:56 +0800

    added eruption frequency bar graph

---
+++ js/bar.js
@@ -0,0 +1,64 @@
+var margin = {
+	top : 20,
+	right : 20,
+	bottom : 30,
+	left : 40
+}, width = 660 - margin.left - margin.right, height = 300 - margin.top - margin.bottom;
+
+var x = d3.scale.ordinal()
+    .rangeRoundBands([0, width], .1);
+
+var y = d3.scale.linear()
+    .range([height, 0]);
+
+var xAxis = d3.svg.axis()
+    .scale(x)
+    .orient("bottom");
+
+var yAxis = d3.svg.axis()
+    .scale(y)
+    .orient("left");
+
+var svg = d3.select("#supplementary_graph").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+d3.csv("data/eruption-count.csv", function(error, data) {
+
+  data.forEach(function(d) {
+    d.frequency = +d.frequency;
+  });
+
+  x.domain(data.map(function(d) { return d.range; }));
+  y.domain([0, d3.max(data, function(d) { return d.frequency; })]);
+
+  svg.append("g")
+      .attr("class", "x axis")
+      .style("stroke", "white")      
+      .attr("transform", "translate(0," + height + ")")
+      .call(xAxis);
+
+  svg.append("g")
+      .attr("class", "y axis")      
+      .style("stroke", "white")   
+      .call(yAxis)
+    .append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 6)
+      .attr("dy", ".71em")
+      .style("stroke", "white")      
+      .style("text-anchor", "end")
+      .text("Frequency");
+
+  svg.selectAll(".bar")
+      .data(data)
+    .enter().append("rect")
+      .attr("class", "bar")
+      .attr("x", function(d) { return x(d.range); })
+      .attr("width", x.rangeBand())
+      .attr("y", function(d) { return y(d.frequency); })      
+      .attr("height", function(d) { return height - y(d.frequency); });
+
+});
